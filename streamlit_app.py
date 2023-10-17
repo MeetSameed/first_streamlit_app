decf115b1902088b1501f529633d6b1fa029c559
MeetSameed <148212444+MeetSameed@users.noreply.github.com>
2023-10-17 16:22:29 +0530

    Update streamlit_app.py
    added breakmenu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healty Diner')
 
-streamlit.title('BreakFast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
