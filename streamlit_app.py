c5f5162e1b7f221f746558b519a19f672ec0c2df
MeetSameed <148212444+MeetSameed@users.noreply.github.com>
2023-10-22 21:03:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 
-import requests
 
 streamlit.title("My Mom's New Healty Diner")
 
@@ -24,5 +23,6 @@ frutis_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(frutis_to_show)
 
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
