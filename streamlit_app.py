d2e8525cc107ab85ae607fe61bd033655d9c25c3
MeetSameed <148212444+MeetSameed@users.noreply.github.com>
2023-10-23 08:13:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title("My Mom's New Healty Diner")
-
 streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -26,18 +25,17 @@ frutis_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(frutis_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+      fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.error("Please select furit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
