a6dd8e120bddc51c2a6d9d176bf440e0f31a77e7
MeetSameed <148212444+MeetSameed@users.noreply.github.com>
2023-10-23 08:16:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(frutis_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-      fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+      fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
           streamlit.error("Please select furit to get information.")
   else:
